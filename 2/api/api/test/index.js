bc05cabd7deec86bb8cc26af2c62f801843feb28
Jon Simpson <simpsonjon@simpsonjon-macbookpro.roam.corp.google.com>
2018-04-26 10:52:03 -0700

    Failure added in text on purpose

--- 2/api/api/test/index.js
+++ 2/api/api/test/index.js
@@ -5,7 +5,7 @@ var tape = require('tape')
 tape('Server returns hello world', function (t) {
   supertest(app)
     .get('/')
-    .expect(200, 'Hello World!')
+    .expect(200, 'Hello World Failure!')
     .end(function (e) {
       t.error(e, 'Supertest checks pass')
       t.end()
