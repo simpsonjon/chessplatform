0b332c4b5262532882eca6d982d9899be3485a02
Jon Simpson <simpson.jon@gmail.com>
2018-05-18 22:45:41 -0700

    Add basic pubsub to chess, close #2

--- 3/chess/chess/index.js
+++ 3/chess/chess/index.js
@@ -1,7 +1,42 @@
 var express = require('express')
+var PubSub = require('@google-cloud/pubsub');
 var app = express()
 
 var response = 'Chess response!'
+var projName = process.env.PROJNAME || 'fauxpassproj-dev'
+var subscriptionName = process.env.SUBNAME || 'moves'
+
+var pubsub = new PubSub({
+  projectId: projName
+})
+/** 
+ * Handle pubsub message pulling and ACKs
+ */
+// References an existing subscription
+
+const subscription = pubsub.subscription(subscriptionName);
+
+// Create an event handler to handle messages
+let messageCount = 0;
+const messageHandler = message => {
+  console.log(`Received message ${message.id}:`);
+  console.log(`\tData: ${message.data}`);
+  console.log(`\tAttributes: ${message.attributes}`);
+  messageCount += 1;
+
+  // "Ack" (acknowledge receipt of) the message
+  message.ack();
+};
+// Listen for new messages until timeout is hit
+subscription.on(`message`, messageHandler);
+setTimeout(() => {
+  subscription.removeListener('message', messageHandler);
+  console.log(`${messageCount} message(s) received.`);
+}, 100 * 1000);
+
+/**
+ * Handle Endpoint for service
+ */
 
 app.get('/', function (req, res) {
   res.send(response)
@@ -10,7 +45,7 @@ app.get('/', function (req, res) {
 var server = null
 
 /* istanbul ignore next */
-function onListen () {
+function onListen() {
   var host = server.address().address
   var port = server.address().port
 
@@ -18,7 +53,7 @@ function onListen () {
 }
 
 /* istanbul ignore next */
-if(module.parent) {
+if (module.parent) {
   module.exports = app
 } else {
   server = app.listen(3000, onListen)
