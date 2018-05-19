1888eb7edb0f39655a3d9500c605ec8c85f49b1f
Jon Simpson <simpson.jon@gmail.com>
2018-05-18 23:31:53 -0700

    Add /login /signup with datastore usage

--- 3/api/api/index.js
+++ 3/api/api/index.js
@@ -1,11 +1,69 @@
 var express = require('express')
 var app = express()
 var request = require('request')
+const Datastore = require('@google-cloud/datastore');
 
 var response = 'Hello World!'
+var projName = process.env.PROJNAME || 'fauxpassproj-dev'
+var topicName = process.env.TOPICNAME || 'chess-moves'
+var nameSpace = process.env.NAMESPACE || 'testing'
 
-app.get('/', function(req,res){
+// Your Google Cloud Platform project ID
+const projectId = 'YOUR_PROJECT_ID';
+
+// Creates a client
+const datastore = new Datastore({
+  projectId: projName,
+});
+
+app.get('/', function (req, res) {
   res.send(response)
+})
+
+app.get('/login/:username', function (req, res) {
+  const kind = 'User'
+  const name = req.params.username
+  const userKey = datastore.key([kind, name])
+
+  datastore
+    .get(userKey)
+    .then((entity) => {
+      if(entity[0] != undefined){
+        res.status(200).send(`Entity exists ${entity[0].registered}`)
+      }else{
+        res.status(403).send(`User not registered`)
+      }
+    })
+    .catch(err => {
+      res.status(500).send(`Error checking for user in datastore`)
+      console.error('ERROR:', err);
+    });
+})
+
+app.get('/signup/:username', function (req, res) {
+  const kind = 'User';
+  const name = req.params.username
+  const userKey = datastore.key([kind, name]);
+
+  // Prepares the new entity
+  const user = {
+    key: userKey,
+    data: {
+      registered: true,
+    },
+  };
+
+  // Saves the entity
+  datastore
+    .save(user)
+    .then(() => {
+      res.status(200).send(`Saved ${user.key.name}`)
+      console.log(`Saved ${user.key.name}: ${user.data.registered}`);
+    })
+    .catch(err => {
+      res.status(500).send(`Error saving: ${user.key.name}`)
+      console.error('ERROR:', err);
+    });
 })
 
 app.get('/move/:id', function (req, res) {
@@ -16,7 +74,7 @@ app.get('/move/:id', function (req, res) {
 })
 
 app.get('/auth/:id', function (req, res) {
-  request('http://auth:3000/'+req.params.id, function (error, response, body) {
+  request('http://auth:3000/' + req.params.id, function (error, response, body) {
     console.log('To auth:' + response.body)
     res.json(JSON.parse(response.body))
   });
@@ -25,7 +83,7 @@ app.get('/auth/:id', function (req, res) {
 var server = null
 
 /* istanbul ignore next */
-function onListen () {
+function onListen() {
   var host = server.address().address
   var port = server.address().port
 
@@ -33,7 +91,7 @@ function onListen () {
 }
 
 /* istanbul ignore next */
-if(module.parent) {
+if (module.parent) {
   module.exports = app
 } else {
   server = app.listen(3000, onListen)
