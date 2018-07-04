68fc0d5b7a654d07faab4fb41d53ed752273c5cb
Jon Simpson <simpson.jon@gmail.com>
2018-07-04 16:53:57 -0700

    Generate SA keys and push to kubectl as secrets

--- 3/make-service-accounts.sh
+++ 3/make-service-accounts.sh
@@ -6,8 +6,9 @@
 gcloud iam service-accounts list | grep -o 'auth-role@\S*\b';
 if [ $? -eq 1 ]; then
 	echo "Making service account auth-role"
-	gcloud iam service-accounts create auth-role --display-name "Auth SA"
 	PROJECT=$(gcloud config get-value project)
+	gcloud iam service-accounts create auth-role --display-name "Auth SA" && \
+    gcloud iam service-accounts keys create auth-key.json --iam-account=auth-role@"$PROJECT".iam.gserviceaccount.com
 	gcloud projects add-iam-policy-binding "$PROJECT" \
 	--member serviceAccount:auth-role@"$PROJECT".iam.gserviceaccount.com --role roles/datastore.user
 	gcloud projects add-iam-policy-binding "$PROJECT" \
@@ -18,8 +19,13 @@ fi
 gcloud iam service-accounts list | grep -o 'chess-role@\S*\b';
 if [ $? -eq 1 ]; then
 	echo "Making service account chess-role"
-	gcloud iam service-accounts create chess-role --display-name "Chess SA"
 	PROJECT=$(gcloud config get-value project)
+	gcloud iam service-accounts create chess-role --display-name "Chess SA" && \
+    gcloud iam service-accounts keys create chess-key.json --iam-account=chess-role@"$PROJECT".iam.gserviceaccount.com
 	gcloud projects add-iam-policy-binding "$PROJECT" \
 	--member serviceAccount:chess-role@"$PROJECT".iam.gserviceaccount.com --role roles/pubsub.subscriber
 fi
+
+echo "Attempting to push keys as secrets through kubectl"
+kubectl create secret generic auth-cred --from-file=./auth-key.json
+kubectl create secret generic chess-cred --from-file=./chess-key.json
