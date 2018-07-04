b9dbfec2df8a54dfd5bf2527b8537cad8dc380d3
Jon Simpson <simpson.jon@gmail.com>
2018-07-04 13:54:38 -0700

    Add auth role to bash script

---
+++ 3/make-service-accounts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This is a temporary method of generating service accounts for the different internal
+# services, it will be replaced eventually by some IaC variant like terraform. 
+
+# Check if auth-role@ exists, if not create it. 
+gcloud iam service-accounts list | grep -o 'auth-role@\S*\b';
+if [ $? -eq 1 ]; then
+	echo "Making service account auth-role"
+	gcloud iam service-accounts create auth-role --display-name "Auth SA"
+	PROJECT=$(gcloud config get-value project)
+	gcloud projects add-iam-policy-binding "$PROJECT" \
+	--member serviceAccount:auth-role@"$PROJECT".iam.gserviceaccount.com --role roles/editor
+fi
