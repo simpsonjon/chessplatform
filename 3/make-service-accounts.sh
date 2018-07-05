43cd14cfd08e69fb2fc3cea5b7a3f63543b84f1f
Jon Simpson <simpson.jon@gmail.com>
2018-07-05 13:24:56 -0700

    MakeServiceAccounts isolate key generation and file checks

--- 3/make-service-accounts.sh
+++ 3/make-service-accounts.sh
@@ -8,7 +8,6 @@ if [ $? -eq 1 ]; then
 	echo "Making service account auth-role"
 	PROJECT=$(gcloud config get-value project)
 	gcloud iam service-accounts create auth-role --display-name "Auth SA" && \
-    gcloud iam service-accounts keys create auth-key.json --iam-account=auth-role@"$PROJECT".iam.gserviceaccount.com
 	gcloud projects add-iam-policy-binding "$PROJECT" \
 	--member serviceAccount:auth-role@"$PROJECT".iam.gserviceaccount.com --role roles/datastore.user
 	gcloud projects add-iam-policy-binding "$PROJECT" \
@@ -21,11 +20,18 @@ if [ $? -eq 1 ]; then
 	echo "Making service account chess-role"
 	PROJECT=$(gcloud config get-value project)
 	gcloud iam service-accounts create chess-role --display-name "Chess SA" && \
-    gcloud iam service-accounts keys create chess-key.json --iam-account=chess-role@"$PROJECT".iam.gserviceaccount.com
 	gcloud projects add-iam-policy-binding "$PROJECT" \
 	--member serviceAccount:chess-role@"$PROJECT".iam.gserviceaccount.com --role roles/pubsub.subscriber
 fi
 
+if [ ! -f ./auth-key.json ]; then
+    gcloud iam service-accounts keys create auth-key.json --iam-account=auth-role@"$PROJECT".iam.gserviceaccount.com
+	echo "Generating Auth-Key as auth-key.json"
+fi
+if [ ! -f ./chess-key.json ]; then
+    gcloud iam service-accounts keys create chess-key.json --iam-account=chess-role@"$PROJECT".iam.gserviceaccount.com
+	echo "Generating Chess-Key as chess-key.json"
+fi
 echo "Attempting to push keys as secrets through kubectl"
 kubectl create secret generic auth-cred --from-file=./auth-key.json
 kubectl create secret generic chess-cred --from-file=./chess-key.json
